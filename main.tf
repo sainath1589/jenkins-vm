9b1cb7c9993651d0b0200edfce12159d916b1005
sainath1589 <132442565+sainath1589@users.noreply.github.com>
2025-01-03 16:37:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,7 @@
-# Define the Google provider
-provider "google" {
-  project     = var.project_id  # Reference the variable for project ID
-  region      = "us-central1"   # Specify the region
-  # Pass service account credentials from variables
+resource "google_storage_bucket" "my-bukcet" {
+  name = "devaramunginta"
+  project = "future-lane-444809-c3"
+  location = "US"
+  force_destory = true
+  public_access_prevention = "enforced"
 }
-
-
-
-# Variable for GCP Project ID
-variable "project_id" {
-  description = "The GCP Project ID"
-  type        = string
-  default     = "future-lane-444809-c3" # Default project ID
-}
-
-# Resource definition for a Compute Engine instance
-resource "google_compute_instance" "default" {
-  name         = "example-vm"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-
-  # Boot disk configuration
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11" # Specify the Debian 11 image
-    }
-  }
-
-  # Network interface configuration
-  network_interface {
-    network       = "default" # Use the default network
-    access_config {}          # Enable external access (NAT)
-  }
-
-  # Instance tags for optional firewall rules
-  tags = ["http-server", "https-server"]
-}
-
-
